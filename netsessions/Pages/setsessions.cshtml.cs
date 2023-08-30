36385f2b5067ac04ae0a50ee05bb6126383868fc
Silvio Chillè <chillesilvio@gmail.com>
2023-08-30 09:58:14 +0200

    Aggiungere i file di progetto.

---
+++ netsessions/Pages/setsessions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace netsessions.Pages.Shared
+{
+    public class setsessionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
