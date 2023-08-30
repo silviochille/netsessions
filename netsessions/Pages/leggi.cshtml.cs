6786ec38552011cf549b234d0321d21aab388bfa
Silvio Chillè <chillesilvio@gmail.com>
2023-08-30 13:15:56 +0200

    seconda versione

--- netsessions/Pages/leggi.cshtml.cs
+++ netsessions/Pages/leggi.cshtml.cs
@@ -9,13 +9,36 @@ namespace netsessions.Pages
         public bool notexists { get; set; }
         public string msg { get; set; }
         public int info { get; set; }
+
+      
+
+      
+
+        public List<string> valoris = new List<string>();
         public void OnGet()
         {
             info = HttpContext.Session.Keys.Count();
-            notexists = string.IsNullOrWhiteSpace(HttpContext.Session.GetString("Chiave"));
+            if(info == 0)
+            notexists = false;
+            else notexists = true;  
 
-            if (notexists)
+            if (!notexists)
                 msg = "Nessun valore di sessione";
+
+            else
+            {
+
+
+                foreach (var key in HttpContext.Session.Keys.ToList())
+                {
+
+                     valoris.Add(HttpContext.Session.GetString(key));
+                    Console.WriteLine(key);
+                }
+
+                Console.WriteLine("bbb");
+                
+            }
 
           
 
